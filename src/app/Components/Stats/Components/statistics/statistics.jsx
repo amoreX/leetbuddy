9cbd74d27b233b481644d1d22e610b881c886bb9
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-09-30 19:21:45 +0530

    add signout-to-title

--- src/app/Components/Stats/Components/statistics/statistics.jsx
+++ src/app/Components/Stats/Components/statistics/statistics.jsx
@@ -1,11 +1,11 @@
 
 import "./statistics.scss"
-
+import { signOut } from "next-auth/react"
 export default function Statistics(){
     return(
         <>
             <div id="statistics-container">
-                <div id="title-stats">
+                <div id="title-stats" onClick={()=>signOut({callbackUrl:"/"})}>
                     <span id="leet-stats">Leet</span>
                     <span id="buddy-stats">Buddy</span>
                 </div>
