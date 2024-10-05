e814bf879338805afb70521d8faf4f642abae863
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-10-06 02:25:54 +0530

    add graph

--- src/app/Components/Stats/Components/statistics/statistics.jsx
+++ src/app/Components/Stats/Components/statistics/statistics.jsx
@@ -1,5 +1,9 @@
 import "./statistics.scss";
 import { signOut } from "next-auth/react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { LineChart } from "@mui/x-charts/LineChart";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { Line } from "react-chartjs-2";
 export default function Statistics() {
   return (
     <>
@@ -12,7 +16,13 @@ export default function Statistics() {
           <div id="questions">
             <div id="easy">
               <span id="number">23</span>
-              <div id="bar" style={{ width: `${(23 / 1726) * 500}%`,backgroundColor:"#28C244"}}></div>
+              <div
+                id="bar"
+                style={{
+                  width: `${(23 / 1726) * 500}%`,
+                  backgroundColor: "#28C244",
+                }}
+              ></div>
               <span id="number">1756</span>
               <span id="caption" style={{ color: "#28C244" }}>
                 Easy
@@ -20,7 +30,13 @@ export default function Statistics() {
             </div>
             <div id="medium">
               <span id="number">50</span>
-              <div id="bar" style={{ width: `${(50 / 1726) * 500}%`,backgroundColor:"#FFA116" }}></div>
+              <div
+                id="bar"
+                style={{
+                  width: `${(50 / 1726) * 500}%`,
+                  backgroundColor: "#FFA116",
+                }}
+              ></div>
               <span id="number">1726</span>
               <span id="caption" style={{ color: "#FFA116" }}>
                 Medium
@@ -28,7 +44,13 @@ export default function Statistics() {
             </div>
             <div id="hard">
               <span id="number">7</span>
-              <div id="bar" style={{ width: `${(7 / 756) * 500}%`,backgroundColor:"#F63737"}}></div>
+              <div
+                id="bar"
+                style={{
+                  width: `${(7 / 756) * 500}%`,
+                  backgroundColor: "#F63737",
+                }}
+              ></div>
               <span id="number">756</span>
               <span id="caption" style={{ color: "#F63737" }}>
                 Hard
@@ -41,6 +63,99 @@ export default function Statistics() {
             <div id="reputation"></div>
             <div id="aceptance"></div>
             <div id="ranking"></div>
+          </div>
+          <div id="chart-container">
+            {/* <Line 
+            data={{
+              area:true,
+              labels:["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+              datasets:[
+                {
+                  label:"Contributions",
+                  data:[2,4,8,2,0,0,1]
+                }
+              ],
+            }}
+          /> */}
+            {/* <LineChart
+              margin={{
+                left: 5,
+                right: 10,
+                top: 30,
+                bottom: 40,
+              }}
+              xAxis={[
+                {
+                  tickLabelStyle: {
+                    fill: "white", // Change the x-axis tick label color to blue
+                  },
+                  type: "ordinal",
+                  data: [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday",
+                  ],
+
+                },
+              ]}
+              yAxis={[
+                {
+                  tickLabelStyle: {
+                    fill: "white", // Change the x-axis tick label color to blue
+                  },
+                },
+              ]}
+              series={[
+                {
+                  data: [2, 5.5, 2, 8.5, 1.5, 5],
+                  area: true,
+                  color: "#28C244",
+                },
+              ]}
+              width={1000}
+              height={300}
+            /> */}
+            <BarChart
+              xAxis={[
+                {
+                  tickLabelStyle: {
+                    fill: "white", // Change the x-axis tick label color to blue
+                  },
+                  scaleType: "band",
+                  data: [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday",
+                  ],
+                },
+              ]}
+              series={[
+                { data: [4, 1, 2, 0, 3, 2, 5] },
+                // { data: [1] },
+                // { data: [2] },
+                // { data: [0] },
+                // { data: [3] },
+                // { data: [2] },
+                // { data: [5] },
+              ]}
+              yAxis={[
+                {
+                  tickLabelStyle: {
+                    fill: "white", // Change the x-axis tick label color to blue
+                  },
+                },
+              ]}
+              width={1000}
+              height={300}
+            />
           </div>
         </div>
       </div>
