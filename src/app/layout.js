03bb2c1dcd93a9984944f66e22c20ab7b6724c6a
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-10-11 14:08:11 +0530

    ported-backend

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        {children}
       </body>
     </html>
   );
