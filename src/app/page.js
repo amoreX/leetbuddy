233732b90f190ed4551ee6f128c626013a1e1439
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-10-11 14:10:13 +0530

    redundant sessions

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 "use client"
 
 import Signin from "./Components/Signin/signin";
-import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 export default function Home() {
-  const { data: session } = useSession();
-  // if (session && session.user) {
-  //   redirect("/Components/Stats");
-  // }
+
+  console.log("wow");
   return (
     <>
       <Signin></Signin>
